82e16c61ce032f7d12c5a61f4fa9af44225a03b0
jill64 <intents.turrets0h@icloud.com>
2024-03-06 15:05:55 +0900

    Refactor fetch logic in load function

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -5,14 +5,15 @@ export const load = async ({ fetch }) => {
   const info = await ec2.get_info()
   const status = info.Reservations?.[0].Instances?.[0]
 
-  const output = status?.PublicDnsName && status.State?.Name === 'running'
-    ? fetch(`https://${status.PublicDnsName}:8000`, {
-        method: 'GET',
-        headers: {
-          'x-secret': EC2_API_KEY
-        }
-      }).then((res) => res.text())
-    : ''
+  const output =
+    status?.PublicDnsName && status.State?.Name === 'running'
+      ? fetch(`https://${status.PublicDnsName}:8000`, {
+          method: 'GET',
+          headers: {
+            'x-secret': EC2_API_KEY
+          }
+        }).then((res) => res.text())
+      : ''
 
   return {
     status,
